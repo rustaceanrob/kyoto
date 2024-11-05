8a894d8030c558c1bb0e21b32a4bdd6a552b5408
rustaceanrob <rob.netzke@gmail.com>
2024-11-05 10:14:29 -1000

    doc: update feature list

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,16 @@
 //! # Getting started
 //!
 //! The [`core`] module documentation is likely the best place to start when developing an application with Kyoto.
+//!
+//! # Features
+//!
+//! `dns`: if no peers are provided, query DNS seeds for Bitcoin nodes to connect to. Default and recommend feature.
+//!
+//! `database`: use the default `rusqlite` database implementations. Default and recommend feature.
+//!
+//! `filter-control`: check filters and request blocks directly. Recommended for silent payments or strict chain ordering implementations.
+//!
+//! `tor` *No MSRV guarantees*: connect to nodes over the Tor network.
 
 #![warn(missing_docs)]
 pub mod chain;
